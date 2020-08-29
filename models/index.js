8e38d9d2f7d93ad068d8c041cbf278258f6a9910
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-29 09:09:11 -0500

    created one to one and one to many relationships

--- models/index.js
+++ models/index.js
@@ -4,13 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category <-- Start Here
+// Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
